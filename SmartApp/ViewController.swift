00bc57c345bbb32299219db22cf9d74b8da79890
Svetleen <gsvetleen@gmail.com>
2018-09-01 21:00:46 -0400

    Initial Commit

---
+++ SmartApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartApp
+//
+//  Created by Svetleen Guerrier on 9/1/18.
+//  Copyright © 2018 ByteHacks2018. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
